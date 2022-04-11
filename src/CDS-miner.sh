9714ac048f883c7f766230d075d6436e6e181858
Pascal Hablützel <68325528+pascalhabluetzel@users.noreply.github.com>
2022-04-11 17:17:59 +0200

    Update CDS-miner.sh

--- src/CDS-miner.sh
+++ src/CDS-miner.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 for file in *.fasta ; do
-# Find gene using exonerator
+# Find gene using exonerate
 exonerate --model est2genome --percent 90 -q query.fa -t "$file" --verbose 0 --showvulgar no --showsugar no --showcigar no --showalignment no --showtargetgff yes --showquerygff no > "$file".gff
 
 
-# Convert gff2 file into a gff3 file
+# Convert gff2 file from exonerate into a gff3 file that can be read by bedtools
 python convert_exonerate_gff_to_gff3.py -i "$file".gff -o "$file".gff3
 
-# Replace "Target" with "Alias"
+
+# Replace "Target" with "Alias" (bedtools complains when the target does not have the expected number of attributes; renaming it 'alias' is a quick and dirty work-around for this issue)
 awk 'gsub("Target","Alias")' "$file".gff3 > "$file"2.gff3
 
 
-# Extract cds using bedtools
+# Extract cds using bedtools (results in each exon appearing on as a separate sequence)
 bedtools getfasta -fi "$file" -bed "$file"2.gff3 -name -s > "$file"_int1.fa
 
 
-# Modify sequence names using awk
+# Modify sequence names using awk (remove the last part of the sequence name, so that sequences from the same contig have the same name)
 awk -F: 'NF>3{$0=$1 FS $2 FS $3} 1' "$file"_int1.fa > "$file"_int2.fa
 
 
-# Concatenate sequence names using awk
+# Concatenate sequence names using awk (appending sequences from the same contig; expect problems when more than one CDS has been found on a single contig)
 awk '
     />/  { id = $0 }
     !/>/ { seq[id] = seq[id] $0 }
