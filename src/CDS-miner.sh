cb45295eb23c8b3c1830e0de8db537b08758b54c
Pascal Habluetzel <pascal.habluetzel@gmail.com>
2022-04-12 09:46:08 +0200

    update

--- src/CDS-miner.sh
+++ src/CDS-miner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for file in *.fasta ; do # Iterate over all genome assemblies in the working directory
+mkdir intermediate_results
+mkdir mined_sequences
+
+for file in *.fna ; do # Iterate over all genome assemblies in the working directory
+
 # Find gene using exonerate
 exonerate --model est2genome --percent 90 -q query.fa -t "$file" --verbose 0 --showvulgar no --showsugar no --showcigar no --showalignment no --showtargetgff yes --showquerygff no > "$file".gff
 
@@ -28,5 +32,14 @@ awk '
     END  { for (id in seq) print id "\n" seq[id] }
 ' "$file"_int2.fa \
 > "$file"_final_result.fa
+
+# Clean up working directory
+mv "$file".gff ./intermediate_results/"$file".gff 
+mv "$file".gff3 ./intermediate_results/"$file".gff3 
+mv "$file"2.gff3 ./intermediate_results/"$file"2.gff3 
+mv "$file"_int1.fa ./intermediate_results/"$file"_int1.fa 
+mv "$file"_int2.fa ./intermediate_results/"$file"_int2.fa 
+mv "$file".fai ./intermediate_results/"$file".fai
+mv "$file"_final_result.fa ./mined_sequences/"$file"_final_result.fa
 
 done
